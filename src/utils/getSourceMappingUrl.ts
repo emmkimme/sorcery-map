183fca36f0785b4e2ef332e5a2ed4015d65ba5a7
emmanuel.kimmerlin@thomsonreuters.com <emmanuel.kimmerlin@thomsonreuters.com>
2022-06-16 14:34:04 +0200

    Add comments

--- src/utils/getSourceMappingUrl.ts
+++ src/utils/getSourceMappingUrl.ts
@@ -2,10 +2,16 @@ import { sourceMappingURLRegex } from './sourceMappingURL';
 
 /** @internal */
 export function getSourceMappingUrl ( str: string ): string | null {
-    if ( !str ) return null;
+    if ( !str ) {
+        return null;
+    }
 
     const candidatsRegExp = sourceMappingURLRegex.exec(str);
-    const candidats = candidatsRegExp ? candidatsRegExp.filter(candidat => candidat).map(candidat => candidat.replace(/\r?\n|\r/g, '')).filter(candidat => candidat.length) : null;
-    const url = candidats && candidats.length > 1 ? candidats[candidats.length - 1].trim() : null;
+    if (!candidatsRegExp) {
+        return null;
+    }
+    // Clean up ahead !!
+    const candidats = candidatsRegExp.map(candidat => candidat && candidat.replace(/\r?\n|\r/g, '').trim()).filter(candidat => candidat && candidat.length);
+    const url = candidats && candidats.length > 1 ? candidats[candidats.length - 1] : null;
     return url;
 }
