c851b9f6655787cfd5f514f34f14e1ccb677d6dc
emmanue.kimmerlin@lseg.com <emmanue.kimmerlin@lseg.com>
2023-03-31 17:08:55 +0200

    add esbuild-plugin

--- src/esbuild/plugin.ts
+++ src/esbuild/plugin.ts
@@ -6,27 +6,23 @@ import { Options, JS_FILE_REGEXP } from '../Options';
 import { ChainInternal } from '../ChainInternal';
 import { Context } from '../Context';
 
-export class Plugin implements ESBuildPlugin {
-    public name: 'sourcery-map';
-    private _options: Options;
-
-    constructor ( options: Options ) {
-        this._options = options;
-    }
-
-    setup ( build: PluginBuild ): void | Promise<void> {
-        build.onLoad({ filter:JS_FILE_REGEXP }, ({ path: file }) => {
-            const context = new Context( path.resolve(), this._options );
-            return ChainInternal.Load( context, file ).then( ( chain ) => {
-                const onLoadResult: OnLoadResult = {};
-                if ( chain ) {
-                    const { content, map } = chain.getContentAndMap( );
-                    if ( !map && content ) {
-                        onLoadResult.contents = content;
+export function Plugin ( options: Options ): ESBuildPlugin {
+    return {
+        name: 'sourcery-map',
+        setup ( build: PluginBuild ): void | Promise<void> {
+            build.onLoad({ filter: JS_FILE_REGEXP }, ({ path: file }) => {
+                const context = new Context( path.resolve(), options );
+                return ChainInternal.Load( context, file ).then( ( chain ) => {
+                    const onLoadResult: OnLoadResult = {};
+                    if ( chain ) {
+                        const { content, map } = chain.getContentAndMap( );
+                        if ( !map && content ) {
+                            onLoadResult.contents = content;
+                        }
                     }
-                }
-                return onLoadResult;
+                    return onLoadResult;
+                });
             });
-        });
-    }
+        }
+    };
 }
